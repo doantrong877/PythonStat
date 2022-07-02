cdd8f135901ba4e460ed464c4956da964ce7ec45
Trong Doan <doantrong8777@gmail.com>
2022-07-02 13:45:10 -0700

    update user

--- fundamentals/fundamentals/user.py
+++ fundamentals/fundamentals/user.py
@@ -13,16 +13,19 @@ class User:
         print("User's age: " + str(self.age))
         print("Reward Member: " + str(self.is_rewards_member))
         print("Card point: " + str(self.gold_card_points))
+        return self
     
     def enroll(self):
         self.is_rewards_member = True
         self.gold_card_points = 200
+        return self
 
     def spend_points(self, amount):
         if self.gold_card_points > amount:
             self.gold_card_points = self.gold_card_points - amount
         else:
             print(f" Hey {self.first_name} You dont have enough point")
+        return self
 
     def member_checker(self):
         if self.is_rewards_member == True:
@@ -30,15 +33,9 @@ class User:
             return False
         return True
 
-trong = User("Trong","Doan","trong@gmail.com", 24)
-trong.display_info()
-trong.enroll()
+
+
 ethan = User("Ethan","Doan","ethan@gmail", 24)
-doan = User("Doan", "Doan","doan@gmail.com", 24)
-trong.spend_points(50)
-ethan.enroll()
-ethan.spend_points(80)
-trong.display_info()
-ethan.display_info()
-doan.display_info()
-doan.spend_points(40)
+
+
+ethan.enroll().spend_points(80).display_info()
