fbb84d9ae116efbd76ec2da69fb4c857cc1d44e6
Trong Doan <doantrong8777@gmail.com>
2022-07-05 14:51:59 -0700

    finish add 2  function

--- flask/fundamental/Counter/server.py
+++ flask/fundamental/Counter/server.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 app.secret_key ="bimat"
 @app.route('/')         
 def index():
-    if 'counter' in session:
-        session['count'] += 1
+    if 'count' in session:
+        if "add" in session:
+            session['count'] += 2
+            session.pop('add')
+        else:
+            session['count'] += 1
         return render_template('index.html', counter = session['count'])
     else:
         session['count'] = 1
@@ -22,9 +26,9 @@ def reset_num():
     return redirect('/')
 
 @app.route('/add')
-def count_num():
-    session['count'] += 2
-    return render_template("index.html", counter = session['count'])
+def add_num():
+    session["add"] = True
+    return redirect('/')
 
 if __name__=="__main__":   
     app.run(debug=True)    
