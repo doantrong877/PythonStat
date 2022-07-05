70a190b554237fb95c5fc40717b72b555b672d98
Trong Doan <doantrong8777@gmail.com>
2022-07-05 14:22:34 -0700

    finished html and server

--- flask/fundamental/Counter/server.py
+++ flask/fundamental/Counter/server.py
@@ -1,28 +1,25 @@
-from flask import Flask, render_template,redirect, session
+from flask import Flask, render_template, request, redirect, session
 
-app = Flask(__name__)
+app = Flask(__name__)  
 app.secret_key ="bimat"
-session['counter'] = 0
+@app.route('/')         
+def index():
+    if 'counter' in session:
+        session['count'] += 1
+        return render_template('index.html', counter = session['count'])
+    else:
+        session['count'] = 1
+        return render_template('index.html', counter = session['count'])
 
-@app.route('/')
-def get_info():
-    session['counter'] += 1
-    return render_template('index.html',count = session['counter'] )
-    
+@app.route('/count')
+def count_num():
+    session['count'] += 1
+    return render_template("index.html", counter = session['count'])
 
-@app('/click', methods =['POST'])
-def click():
-    session['counter'] += 1
-    return redirect('/count')
-
-@app('/count')
-def count():
-    return render_template('index.html',count = session['counter'])
-
-@app('/reset', methods = ['POST'])
-def reset():
-    session['counter'] = 0
+@app.route('/reset')
+def reset_num():
+    session.clear()
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug == True)
+if __name__=="__main__":   
+    app.run(debug=True)    
