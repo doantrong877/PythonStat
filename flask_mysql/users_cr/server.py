2ef3ed246c37baa8c4f5b0d238cd4124409820c9
Trong Doan <doantrong8777@gmail.com>
2022-07-12 12:19:30 -0700

    back end works

--- flask_mysql/users_cr/server.py
+++ flask_mysql/users_cr/server.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, request
 
 from user import User
 
-app = Flask(__name__)
 
-@app.route("/")
+app = Flask(__name__)
+app.secret_key = "bimat"
+
+@app.route("/users")
 def index():
     users = User.get_all()
     return render_template("read.html", users = users)
+
+@app.route('/user/new')
+def create():
+    return render_template("create.html")
+
+@app.route('/create', methods =['POST'])
+def user_input():
+    data = {
+        'fname' : request.form['fname'],
+        'lname' : request.form['lname'],
+        'email' : request.form['email'],
+            }
+
+    User.save(data)
+   
+    return redirect('/users')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
