5476c356533fb6c6a90a641809667149a7c9f663
Trong Doan <doantrong8777@gmail.com>
2022-07-12 11:49:54 -0700

    finish pull data from db

---
+++ flask_mysql/users_cr/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+from user import User
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    users = User.get_all()
+    return render_template("read.html", users = users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- flask_mysql/users_cr/user.py
+++ flask_mysql/users_cr/user.py
@@ -1,11 +1,12 @@
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
-class Friend:
+class User:
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
+        self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
