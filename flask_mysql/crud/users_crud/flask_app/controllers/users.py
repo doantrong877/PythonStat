9f572d788f9cebfcd3edab52aa71e6e85b858b82
Trong Doan <doantrong8777@gmail.com>
2022-07-12 17:49:09 -0700

    update MVC model

---
+++ flask_mysql/crud/users_crud/flask_app/controllers/users.py
@@ -0,0 +1,61 @@
+from flask_app import app
+from flask import render_template,redirect,request,session,flash
+from flask_app.models.user import User
+@app.route("/")
+def index():
+    users = User.get_all()
+    return redirect('/users')
+
+@app.route("/users")
+def users():
+    users = User.get_all()
+    return render_template("read.html", users = users)
+
+@app.route('/user/new')
+def create():
+    return render_template("create.html")
+
+@app.route('/create', methods =['POST'])
+def user_input():
+    data = {
+        'fname' : request.form['fname'],
+        'lname' : request.form['lname'],
+        'email' : request.form['email']
+            }
+
+    User.save(data)
+   
+    return redirect('/users')
+
+@app.route('/delete/<int:id>')
+def delete_user(id):
+    data = {"id" : id}
+    User.delete(data)
+    return redirect("/users")
+
+@app.route('/show/<int:id>')
+def display_user(id):
+    data = {"id" : id}
+    user = User.get_user(data)
+    return render_template('info.html', user = user)
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    data = {
+        "id" : id
+    }
+    user = User.get_user(data)
+    return render_template('edit.html', user = user)
+
+
+@app.route('/update', methods =['POST'])
+def update():
+    data = {
+        'fname' : request.form['fname'],
+        'lname' : request.form['lname'],
+        'email' : request.form['email'],
+            }
+
+    User.edit(data)
+   
+    return redirect('/users')
--- flask_mysql/crud/users_crud/server.py
+++ flask_mysql/crud/users_crud/server.py
@@ -1,69 +1,13 @@
-from flask import Flask, render_template, redirect, session, request
 
-from user import User
+from flask_app.controllers import users
 
-
-app = Flask(__name__)
-app.secret_key = "bimat"
-
-@app.route("/")
-def index():
-    users = User.get_all()
-    return redirect('/users')
-
-@app.route("/users")
-def users():
-    users = User.get_all()
-    return render_template("read.html", users = users)
-
-@app.route('/user/new')
-def create():
-    return render_template("create.html")
-
-@app.route('/create', methods =['POST'])
-def user_input():
-    data = {
-        'fname' : request.form['fname'],
-        'lname' : request.form['lname'],
-        'email' : request.form['email']
-            }
-
-    User.save(data)
-   
-    return redirect('/users')
-
-@app.route('/delete/<int:id>')
-def delete_user(id):
-    data = {"id" : id}
-    User.delete(data)
-    return redirect("/users")
-
-@app.route('/show/<int:id>')
-def display_user(id):
-    data = {"id" : id}
-    user = User.get_user(data)
-    return render_template('info.html', user = user)
-
-@app.route('/edit/<int:id>')
-def edit(id):
-    data = {
-        "id" : id
-    }
-    user = User.get_user(data)
-    return render_template('edit.html', user = user)
+from flask_app import app
 
 
-@app.route('/update', methods =['POST'])
-def update():
-    data = {
-        'fname' : request.form['fname'],
-        'lname' : request.form['lname'],
-        'email' : request.form['email'],
-            }
 
-    User.edit(data)
-   
-    return redirect('/users')
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
