93949975ac81edbb5bf6a97d0fff22db3932f72d
Trong Doan <doantrong8777@gmail.com>
2022-07-14 20:19:08 -0700

    books server

--- flask_mysql/crud/books/flask_app/controllers/books.py
+++ flask_mysql/crud/books/flask_app/controllers/books.py
@@ -1,6 +1,8 @@
+from crypt import methods
 from flask_app import app
 from flask_app.models.book import Book
-from flask import render_template, redirect, request 
+from flask_app.models.author import Author
+from flask import render_template, redirect, request,session
 
 @app.route('/')
 def home():
@@ -18,3 +20,19 @@ def add_book():
     }
     Book.save(data)
     return redirect('/')
+
+@app.route("/book/<int:id>")
+def favo_author(id):
+    data = {
+        "id" : id
+    }
+    return render_template('favoriteauthor.html', book = Book.get_one(data), authors = Author.get_all(), favo = Book.get_book_with_authors(data))
+
+@app.route("/add/favoauthor", methods=['POST'])
+def add_favoauthor():
+    data = {
+        "first_name" : request.form['name']
+    }
+    session['first_name'] = data
+    Author.save(data)
+    return rend
