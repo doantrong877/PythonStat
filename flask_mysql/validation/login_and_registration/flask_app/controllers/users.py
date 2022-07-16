f4a840e040d4e5fbe5aa5a846e7721ff07a9bb1e
Trong Doan <doantrong8777@gmail.com>
2022-07-16 16:45:10 -0700

    finish login and registration

--- flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
+++ flask_mysql/validation/login_and_registration/flask_app/controllers/users.py
@@ -1,25 +1,56 @@
 from flask_app import app
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
 from flask_bcrypt import Bcrypt        
 bcrypt = Bcrypt(app)     
 
 
 
-app.route('/')
+@app.route('/')
 def home():
     return render_template('login.html')
 
-app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def register():
-    if not User.validate_registration(request.form):
+    if request.form['password'] != request.form['confirm_password']:
+        flash("passwords doesn't match", 'register') 
+        return redirect('/')
+    if not User.validate_registration(request.form):       
         return redirect('/')
     pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data =  {
+        "first_name" : request.form['first_name'],
+        "last_name" : request.form['last_name'],
+        "email" : request.form['email'],
+        'password' : pw_hash
+    }
+    user = User.save(data)
+    session ['user_info'] = f"{request.form['first_name']} {request.form['last_name']}"
+    session['email'] = request.form['email']
+    return redirect('/dashboard')
 
-    User.save(request.form)
-
-    userinfo = User.get_by_email(request.form['email'])
-
+@app.route('/login', methods=['POST'])
+def login():
+    data = {'email': request.form['email']}
+    user = User.get_by_email(data)
+    if not user :
+        flash("Email has not been registed","login")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid Email/Password","login")
+        return redirect('/')
+    session['user_id'] = user.id
+    session ['user_info'] = f"{user.first_name} {user.last_name}"
+    return redirect('/dashboard')
     
 
-    return render_template('logout.html', userinfo = userinfo)
+@app.route('/dashboard')
+def dashboard():
+
+    return render_template('logout.html', userinfo = session['user_info'])
+
+@app.route('/logout')
+def logout():
+    session.clear
+    return redirect('/')
+
--- flask_mysql/validation/login_and_registration/flask_app/models/user.py
+++ flask_mysql/validation/login_and_registration/flask_app/models/user.py
@@ -17,14 +17,14 @@ class User:
     @classmethod
     def get_by_email(cls,data):
         query = "SELECT * FROM users WHERE email = %(email)s;"
-        result = connectToMySQL('users_schema').query_db(query,data)
-        if len(result < 1):
+        results = connectToMySQL('users_schema').query_db(query,data)
+        if len(results) < 1:
             return False
-        return cls(result[0])
+        return cls(results[0])
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO users (first_name,last_name,email,password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
+        query = "INSERT INTO users (first_name,last_name,email,password,created_at,updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s,NOW(), NOW());"
         return connectToMySQL('users_schema').query_db(query,data)
 
     @classmethod
@@ -32,24 +32,20 @@ class User:
         is_valid = True
 
         if len(data['first_name']) < 2:
-            flash("First name has two have at least 2 character")
+            flash("First name has two have at least 2 character",'register')
             is_valid = False
         if len(data['last_name']) < 2:
-            flash("Last name has two have at least 2 character")
+            flash("Last name has two have at least 2 character",'register')
             is_valid = False
         if not EMAIL_REGEX.match(data['email']):
-            flash("Invalid email addess!", 'email')
+            flash("Invalid email addess!",'register')
             is_valid = False
-        query = "SELECT * FROM emails"
-        result = connectToMySQL("users_schema").query_db(query)
-        for email_info in result:
-            if cls(email_info).email == data['email']:
-                flash("email is already taken")
-                is_valid = False
-                break
         if len(data['password']) < 8:
-            flash("Password need at least 8 characters")
+            flash("Password need at least 8 characters",'register')
+            is_valid = False
+        if  cls.get_by_email(data) != False:
+            flash("email has already registed",'register')
             is_valid = False
         return is_valid
-
-
+    
+    
